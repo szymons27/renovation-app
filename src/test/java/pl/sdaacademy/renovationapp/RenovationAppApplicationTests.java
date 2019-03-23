8edd30995a3d19075a72fe545ec653cd3a4bc6a7
Szymon Sygiet <sz.sygiet@gmail.com>
2019-03-23 13:54:29 +0100

    first commit

---
+++ src/test/java/pl/sdaacademy/renovationapp/RenovationAppApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.sdaacademy.renovationapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RenovationAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
